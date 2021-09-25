a321dcd0eff1a2808bbdda43ff3825793763b625
Miihir79 <mihirrshah02@gmail.com>
2021-09-26 00:15:49 +0530

    Gantt chart done

--- CpuScheduling.py
+++ CpuScheduling.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def FCFS(AT, BT):
@@ -7,27 +9,63 @@ def FCFS(AT, BT):
     TAT = []  # CT - AT
     WT = []  # TAT - BT
 
-# To convert string type AT to int type
+    # To convert string type AT to int type
     AT = [int(i) for i in AT]
     BT = [int(i) for i in BT]
 
     timeCpu = 0
 
+    yticks = []  # This is to store the bartick values
+    ytickL = []  # This is to store the bartick lables
+
+    barHeightVar = 1
+    fig, gnt = plt.subplots()
+
+    # Setting labels for x-axis and y-axis
+    gnt.set_xlabel('Time')
+    gnt.set_ylabel('Processes')
+
     for i in range(0, len(AT)):
-        print(timeCpu)
         # to make up for idle CPU time
         while AT[i] > timeCpu:
-            print("timeCpu:", timeCpu)
-            print("AT", AT[i])
             timeCpu += 1
         # Calculating CT by adding Current CPU time and BT
+
+        storeThis = timeCpu  # This variable will store the starting time of the i'th process
+
         timeCpu += BT[i]
         CT.append(timeCpu)
 
         TAT.append(CT[i] - AT[i])
         WT.append(TAT[i] - BT[i])
 
+        # print(AT[i], BT[i])
+        gnt.broken_barh([(storeThis, BT[i])], (barHeightVar, 2), facecolors='tab:blue')   # here we want startingTime
+        # of process (storeThis) + The time taken for the process (BT)
+
+        yticks.append(
+            barHeightVar + 1)  # thickness of bars is 2 and base starts at barHeightVar, so +1 will give us middle
+
+        ytickL.append("P{}".format(i + 1))  # formatting to show P1,P2,P3...
+
+        barHeightVar += 3  # thickness is 2 and extra 1 for spacing
+
     print(CT, TAT, WT)
+
+    # Setting Y-axis limits 2 more than the list size
+    gnt.set_ylim(0, (len(AT) + 2) * 3)
+
+    # Setting X-axis limits last plus five
+    gnt.set_xlim(0, CT[-1] + 5)
+
+    # Setting ticks on y-axis
+    gnt.set_yticks(yticks)
+
+    # Labelling tickes of y-axis
+    gnt.set_yticklabels(ytickL)
+    plt.show()
+
+    # variables for table: Process number-ytickL , AT, BT, CT, TAT , WT in the same order.
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -68,7 +106,6 @@ BTList.pack()
 L5 = Button(F1, borderwidth="0", text="Visualise", bg="#e8e8e8", fg="green", font=("Century Gothic", 18),
             activeforeground="black", activebackground="#bbbfca",
             command=lambda: FCFS(ATList.get().split(" "), BTList.get().split(" "))).pack(pady="25")
-# )
 
 L6 = Button(F1, borderwidth="0", text="Compare All Algorithms", bg="#e8e8e8", fg="green", font=("Century Gothic", 18),
             activeforeground="black", activebackground="#bbbfca").pack()
